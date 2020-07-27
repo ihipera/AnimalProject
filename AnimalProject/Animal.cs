8f8fc5d332fc5c5d7160ff64b400bb7a16db0f72
johnil1 <ihipera1@gmail.com>
2020-07-27 13:46:47 +1200

    Add project files.

---
+++ AnimalProject/Animal.cs
@@ -0,0 +1,36 @@
+﻿/* The Animal class
+ * contains the fiels, constructor and method that defines an animal
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalProject
+{
+   public class Animal
+    {
+        //fields, also called private data methods
+        private string type;
+        private string name;
+        private int legs;
+
+        // constructor -- assigns initial values no return type
+        public Animal(string type, string name, int legs)
+        {
+            // right hand side is value passed in
+            this.type = type;
+            this.name = name;
+            this.legs = legs;
+        }
+        // user defined method
+        public string ListDetails()
+        {
+            return ("I am a " + type + ", my name is " + name + " and I have " + legs + " legs.");
+        }
+
+    }
+}
---
+++ AnimalProject/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalProject
+{
+    public partial class Form1 : Form
+    {
+        private Animal dog;
+        private Animal cat;
+        public Form1()
+        {
+            InitializeComponent();
+            dog = new Animal("dog", "Tio", 4);
+            cat = new Animal("cat", "Magic", 2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(dog.ListDetails());
+            MessageBox.Show(cat.ListDetails());
+        }
+    }
+}
